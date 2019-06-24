5b502480ed5b9ef2c82f50470539746c6927e3f4
Natthawut Ruppat <6206021612032@fitm.kmutnb.ac.th>
2019-06-24 23:12:34 +0700

    Create pp.cpp

---
+++ pp.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std ;
+
+void main ()
+{
+	
+
+	int a  ;
+	cout << "Enter Number 0 - 9  : ";
+	cin >> a;
+	
+	cout << "\n";
+	
+	if (a==0)
+	{
+		cout <<" zero" <<  endl ;
+	}
+	else if (a==1)
+	{
+		cout <<" one" <<  endl ;
+	}
+	else if (a==2)
+	{
+		cout <<" two" <<  endl ;
+	}
+	else if (a==3)
+	{
+		cout <<" three" <<  endl ;
+	}
+	else if (a==4)
+	{
+		cout <<" four" <<  endl ;
+	}
+	else if (a==5)
+	{
+		cout <<" five" <<  endl ;
+	}
+	else if (a==6)
+	{
+		cout <<" six" <<  endl ;
+	}
+	else if (a==7)
+	{
+		cout <<" seven" <<  endl ;
+	}
+	else if (a==8)
+	{
+		cout <<" eight" <<  endl ;
+	}
+        else if (a==9)
+	{
+		cout <<" nine" <<  endl ;
+	}
+	else  
+	{
+		cout <<" Please enter number agian " <<  endl ;
+	}
+	cout << "\n";
+}
