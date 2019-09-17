a342eafd1480a85f2070e1d256c40a1bafab18fe
Natthawut Ruppat <52170322+natthawutruppat@users.noreply.github.com>
2019-09-17 20:35:26 +0700

    Update swaparraytranspost.cpp

--- swaparraytranspost.cpp
+++ swaparraytranspost.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 int main(){
 	int a[3][4];
-	for (int i = 1; i<3; i++){
-		for (int j = 1; j<4; j++){
+	for (int i = 1; i<3; i++)
+	{
+		for (int j = 1; j<4; j++)
+		{
 			cout << "Input [" << i << "][" << j << "] : ";
 			cin >> a[i][j];
 		}
